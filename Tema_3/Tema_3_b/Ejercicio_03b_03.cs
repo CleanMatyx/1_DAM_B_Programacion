d07524f907858791cea6dc42dd58b8f2c431551c
Matías Exequiel Borra Quiroz <145360448+CleanMatyx@users.noreply.github.com>
2023-10-11 15:50:30 +0200

    Add files via upload

--- Tema_3/Tema_3_b/Ejercicio_03b_03.cs
+++ Tema_3/Tema_3_b/Ejercicio_03b_03.cs
@@ -2,3 +2,23 @@
  * Programa que pide una variable distancia en Km 'int' y el tiempo necesario
  * para recorrerla (HH,MM,SS) para calcular los metros por segundo.
  */
+ 
+ using System;
+ class Ejercicio_03b_03
+ {
+	static void Main()
+	{
+		int km,h,min,seg,tiempo;
+		double vel;
+		Console.WriteLine("Introduzca la distancia en Km a recorrer: ");
+		km=Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Introduzca el tiempo necesario en Horas, Minutos,"+
+			" Segundos");
+		h=Convert.ToInt32(Console.ReadLine());
+		min=Convert.ToInt32(Console.ReadLine());
+		seg=Convert.ToInt32(Console.ReadLine());
+		tiempo=h*3600+min*60+seg;
+		vel=km+1000.0/tiempo;
+		Console.WriteLine("La velocidad es {0}m/s.",vel);
+	}
+}
