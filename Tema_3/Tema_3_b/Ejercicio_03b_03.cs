f8526fcacc0e3e561830b15d6cfa4273b47176b8
Matías Exequiel Borra Quiroz <145360448+CleanMatyx@users.noreply.github.com>
2023-10-11 16:04:41 +0200

    Update Ejercicio_03b_03.cs
    Mejora de código

--- Tema_3/Tema_3_b/Ejercicio_03b_03.cs
+++ Tema_3/Tema_3_b/Ejercicio_03b_03.cs
@@ -9,16 +9,19 @@
 	static void Main()
 	{
 		int km,h,min,seg,tiempo;
-		double vel;
+		float vel;
 		Console.WriteLine("Introduzca la distancia en Km a recorrer: ");
 		km=Convert.ToInt32(Console.ReadLine());
-		Console.WriteLine("Introduzca el tiempo necesario en Horas, Minutos,"+
-			" Segundos");
+		Console.WriteLine("Tiempo empleado");
+		Console.Write("Horas: ");
 		h=Convert.ToInt32(Console.ReadLine());
+		Console.Write("Minutos: ");
 		min=Convert.ToInt32(Console.ReadLine());
+		Console.Write("Segundos: ");
 		seg=Convert.ToInt32(Console.ReadLine());
 		tiempo=h*3600+min*60+seg;
-		vel=km+1000.0/tiempo;
+		vel=km*1000f/tiempo;
 		Console.WriteLine("La velocidad es {0}m/s.",vel);
 	}
 }
+
